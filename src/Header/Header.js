cef6aa09b98d64b104b302826bee7cbeca1f5d9e
Alexandra Irwin <airwin357@gmail.com>
2025-05-12 08:54:14 +1000

    second commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,27 @@
-function Header() {
+import styles from './Header.module.css';
+import { FaReddit } from "react-icons/fa";
+import { GiSeaTurtle } from "react-icons/gi";
+export function Header() {
     return (
-        <h1>Reddit Minimal</h1>
+        <>
+        <header>
+            <div>
+                <GiSeaTurtle />
+                <FaReddit />
+                 <p>
+                    Reddit<span>Minimal</span>
+                 </p>
+            </div> 
+            <form>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  aria-label= "Search posts"/>
+                <button type="submit" aria-label="Search"></button>  
+            </form>
+              
+
+        </header>
+        </>
     )
 }
