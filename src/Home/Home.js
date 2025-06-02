43b47921ad60f831a8a821cf4b649e11b449a1dc
Alexandra Irwin <airwin357@gmail.com>
2025-06-03 09:05:13 +1000

    another commit

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,37 @@
 import React from 'react';
-
+import myImage from '../images/david-clode-9FvTOCfvt_w-unsplash.jpg';
+import myImage1 from '../images/david-clode-iLwQIbWxv-s-unsplash (1).jpg';
+import myImage2 from "../images/sense-atelier-VKzHzB4vDtQ-unsplash.jpg";
 
 export function Home() {
+   const posts = [ 
+    {
+      id: 0,
+      title: 'sea turtle',
+      img: myImage,
+      author: 'alexandra'
+    },
+    {
+      id: 1,
+      title: 'post title',
+      image: myImage1,
+      author: 'jai'
+    },
+    {
+      id: 2,
+      title: 'my post title',
+      image: myImage2,
+      author: 'elsie'
+    }
+
+   ]
     return (
         <>
         {posts.map((post, index) => (
         <Post
           key={post.id}
           post={post}
-          onToggleComments={onToggleComments(index)}
+          //onToggleComments={onToggleComments(index)}
         />
       ))}
         </>
